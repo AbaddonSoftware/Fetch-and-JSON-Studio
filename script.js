c8cfc25cfd763bac30df0498e576ac6318e270d8
Abaddon <donnie.l.baker@gmail.com>
2020-05-17 12:20:29 -0400

    added active css for active astronauts

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ window.addEventListener("load", function (event) {
             let astronautCount = `${astronauts.length}`;
             astronauts.sort((elementA, elementB) => elementA.hoursInSpace - elementB.hoursInSpace);
             for (let astronaut of astronauts) {
+                let status = astronaut.active ? "active" :
+                    "inactive";
                 container.innerHTML +=
                     `<div class="astronaut">
                     <div class="bio">
                         <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
                         <ul>
                             <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                            <li>Active: ${astronaut.active}</li>
+                            <li class=${status}>Active: ${astronaut.active}</li>
                             <li>Skills: ${astronaut.skills}</li>
                         </ul>
                     </div >
